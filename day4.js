e19566131a6ff599e532033f8b7a652dab32f2bc
Ashley N Dennis <a.n.d@Ashleys-MacBook-Air.local>
2022-01-23 17:33:30 -0500

    added solution and organize day4

--- day4.js
+++ day4.js
@@ -1,34 +1,47 @@
-//1. Write a loop that outputs
-// 10
-// 9
-// 8
-// ...
-// 2
-// 1
-// BlAST OFF!
+/* Problem 1:
+1. Write a loop that outputs
+10
+9
+8
+...
+2
+1
+// BlAST OFF!*/
 
-// const descenNum = [1, 2, 3, 4, 5, 8, 9, 10];
+const descenNum = [1, 2, 3, 4, 5, 8, 9, 10];
 
-// for (const number of descenNum.reverse()) {
-//   console.log(number);
-// }
-// console.log("BLAST OFF!");
+//Example 1:
+for (const number of descenNum.reverse()) {
+  console.log(number);
+}
+console.log("BLAST OFF!");
 
-// for (const num of descenNum) {
-// console.log(num);
+//Example 2:
+for (let i = 10; i > 0; i--) {
+  console.log(i);
+  if (i === 1) {
+    console.log("BLAST OFF!");
+  }
+}
 
-// for (let i=0; i < descenNum.length; i++) {
+//Example 3:
+for (let i=0; i < descenNum.length; i++) {
 
-// }  console.log(...descenNum.reverse() + 'Blast Off!')
+}  console.log(...descenNum.reverse() + 'Blast Off!')
 
-/* 1. create a function that takes an array of numbers
- & return both min and max in that order. (use a loop) */
+//-----------------------------------------------------------------
+/* Problem 2: 
+Create a function that takes an array of numbers
+& return both min and max in that order. (use a loop) */
 
 const arr = [4, 3, 6, 1, 5, 2, 7];
 
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(Math.min(...arr),",", Math.max(...arr));
-// }
+// Example 1:
+for (let i = 0; i < arr.length; i++) {
+  console.log(Math.min(...arr),",", Math.max(...arr));
+}
+
+// Example 2:
 function findMinMax(arr) {
   let min = arr[0];
   let max = arr[0];
@@ -45,13 +58,17 @@ function findMinMax(arr) {
 }
 console.log(findMinMax(arr));
 
-// 1. Converts a string to uppercase letters:
-// let str = "WHAT a TIME to BE ALIVE!"
+//---------------------------------------------------------------
 
-// let str = "WHAT a TIME to BE ALIVE!";
-// console.log(str.toUpperCase());
+// Problem 3: Converts a string to uppercase letters:
+let str = "WHAT a TIME to BE ALIVE!"
 
-// function capsCase(str){
-//     console.log(str.toUpperCase());
-// }
-// console.log(capsCase(str))
+//Example 1
+let str = "WHAT a TIME to BE ALIVE!";
+console.log(str.toUpperCase());
+
+//Example 2
+function capsCase(str){
+    console.log(str.toUpperCase());
+}
+console.log(capsCase(str))
