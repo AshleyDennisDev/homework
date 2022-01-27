7af96e857426e4c4c4645670aba015ea48e74053
Ashley N Dennis <a.n.d@Ashleys-MacBook-Air.local>
2022-01-26 23:30:22 -0500

    removed sumArrs code

--- function.js
+++ function.js
@@ -19,17 +19,3 @@ function addition(a, b) {
 }
 console.log(addition(2, 4));
 
-//----------------------------------------------------------
-
-let arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; // --> 276
-let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; // --> 351
-// Example output: 
-// 627
-
-function sumOfArrs(sum1){
-    let i=0;
-for (i=0; i< arr_1.length; i++){
-     sum1 += arr_1[i];
-} return sum1
-}
-console.log(sumOfArrs(arr_1))
