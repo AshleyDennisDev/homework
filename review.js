af657be2b6731f178a3d1f4053a09e44ac291846
Ashley Dennis <adenn1852@gmail.com>
2022-03-01 22:20:52 -0500

    Uncommented solutions

---
+++ review.js
@@ -0,0 +1,23 @@
+let a= 3
+let b= 8
+let c= 7
+
+function multiplyNum(x,y,z) {
+    return (x*y*z)
+}
+console.log(multiplyNum(a,b,c))
+
+// second example
+let product = (x,y,z) => x*y*z
+
+ console.log(product(a,b,c))
+
+// ##################################################
+ let chickens = 2 
+ let cows = 4 
+ let pigs = 4 
+
+ function animals(x, y, z) {
+    console.log( x + y + z)
+ }
+ animals(chickens, cows, pigs)
